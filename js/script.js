fd9552d98bba5c6f3c2abe3d34be3f6cf27efd05
Ali-Mugamai <113056138+Ali-Mugamai@users.noreply.github.com>
2022-10-19 13:56:37 -0400

    work

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Copyright (c) 2020 Ali Mugamai All rights reserved
+//
+// Created by: Ali Mugamai
+// Created on: oct 2022
+// This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates area and perimeter of rectangle.
+ */
+function calculate () {
+  // input
+  const hight = parseInt(document.getElementById('hight-of-triangle').value)
+  const base = parseInt(document.getElementById('base-of-triangle').value)
+
+  // process
+  const area = 2 / (hight * base)
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+}
