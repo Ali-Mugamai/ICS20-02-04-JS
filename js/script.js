5e8ca20cc14aac7f01c3c1cfa378443e9af14b1f
Ali-Mugamai <113056138+Ali-Mugamai@users.noreply.github.com>
2022-10-19 14:48:20 -0400

    bruh

--- js/script.js
+++ js/script.js
@@ -12,9 +12,8 @@ function calculate() {
   // input
   const height = parseInt(document.getElementById("height-of-triangle").value)
   const base = parseInt(document.getElementById("base-of-triangle").value)
-
   // process
-  const area = 2 / (height * base)
+  const area = (base * height) / 2
   // output
   document.getElementById("area").innerHTML = "Area is: " + area + " cm²"
 }
